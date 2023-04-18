620072aaee53260bb6c35740abec0e66a8983582
ashaansojib <ashaduzzamansojib67@g.gmail.com>
2023-04-19 02:14:11 +0600

    add login form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            <h1>this is register</h1>
+        <div className="card mx-auto max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="text" placeholder="email" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="text" placeholder="password" className="input input-bordered" />
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Login</button>
+                </div>
+            </div>
         </div>
     );
 };
